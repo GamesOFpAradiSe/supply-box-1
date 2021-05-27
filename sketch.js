e5dbbaad0cdfdea0f7323c00d6407f189975e2bf
GamesOFpAradiSe <83218103+GamesOFpAradiSe@users.noreply.github.com>
2021-05-27 14:46:21 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -31,7 +31,7 @@ function setup() {
 	engine = Engine.create();
 	world = engine.world;
 
-	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0, isStatic:true});
+	packageBody = Bodies.circle(width/2 , 200 , 5 , { isStatic:true});
 	World.add(world, packageBody);
 	
 
@@ -42,7 +42,7 @@ function setup() {
 
 	Engine.run(engine);
   
-console.log(packageSprite)
+console.log(packageBody)
 
 }
 
